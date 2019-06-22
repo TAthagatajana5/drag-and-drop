21f87b0ba06ec138f53b4806eb6d2ad71b45e872
Tathagata Jana <tathagatajana5@gmail.com>
2019-06-22 09:17:10 +0530

    initial commit

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPEhtml>
+<html>
+	<head>
+		<title>WELCOME TO DAB</title>
+        <link rel="icon" href="" type="image/x-icon"/>
+        <link rel="shortcut icon" href="" type="image/x-icon"/>
+        <link rel="stylesheet" href="css/home_style.css"/>
+	</head>
+    <body class="main">
+        <div class="upper_banner">
+            <div class="logo">
+                <img class="logo_anchor" src="" alt="logo of the site"/>
+            </div>
+            <div class="upper_middle_space"></div>
+            <div class="register">
+                <a class="register_anchor" href="register_form.php">Register</a>
+            </div>
+            <div class="login">
+                <a class="login_anchor" href="login_form.php">Log-in</a>
+            </div>
+        </div>
+        <div class="body">
+            <div class="left_body"></div>
+            <div class="center-body">
+                <div class="upper_center_body">
+                    <div class="right_upper_center_body">
+                        <a class="catalogue_anchor" href="catalogue.php">Catalogue</a>
+                    </div>
+                    <div class="left_upper_center_body">
+                        <a class="create_layout_anchor" href="create_layout.php">Create Layout</a>
+                    </div>
+                </div>
+                <div class="lower_center_body">
+                    <a class="community_anchor" href="community.php">Community</a>
+                </div>
+            </div>
+            <div class="right_body"></div>
+        </div>
+        <div class="lower_banner">
+            <div class="social_media"></div>
+            <div class="lower_middle_space"></div>
+            <div class="info"></div>
+        </div>
+        <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="js/home_script.js"></script>
+	</body>
+</html>       
